bae55a65730cc54a890923859f052cc257ed9ad7
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2023-11-24 10:15:28 +0100

    update footer

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    
+
+<footer class="mx-0 mb-0 p-0 bg-blue-100 shadow dark:bg-gray-900 m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="images/logo.png" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Avito</span>
+            </a>
+            <ul class="flex flex-wrap items-center space-x-6 mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+                </li>
+                <li>
+                    <a href="users.php" class="hover:underline me-4 md:me-6">Users</a>
+                </li>
+                <li>
+                    <a href="index.php" class="hover:underline me-4 md:me-6">Announces</a>
+                </li>
+                
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-300 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Avito</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
+</body>
+</html>
