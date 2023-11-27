f7b18535559e32164580668d7c8d506e47051ba3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2023-11-27 15:27:41 +0100

    update sign up

--- signup.php
+++ signup.php
@@ -1,45 +1,68 @@
 <?php
-// Vérifier si le formulaire a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Inclure votre fichier de configuration de la base de données
-    include('connection.php');
+// connection.php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "avito";
 
-    // Récupérer les données du formulaire
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
-    $phone = $_POST['phone'];
-    $city = $_POST['city'];
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Hasher le mot de passe avant de le stocker dans la base de données
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    // Préparer et exécuter la requête SQL
-    $sql = "INSERT INTO users (name, email, password, confirm_password, phone, city) VALUES (?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    
-    // Vérifier si la préparation de la requête a réussi
-    if ($stmt) {
-        // Lier les paramètres
-        $stmt->bind_param("sssss", $name, $email, $hashedPassword, $confirm_password, $phone, $city);
-
-        // Exécuter la requête
-        if ($stmt->execute()) {
-            // Rediriger l'utilisateur vers la page de connexion après l'inscription réussie
-            header("Location: sign_in.php");
-            exit();
-        } else {
-            echo "Erreur d'exécution de la requête : " . $stmt->error;
-        }
-
-        // Fermer la déclaration préparée
-        $stmt->close();
-    } else {
-        echo "Erreur de préparation de la requête : " . $conn->error;
-    }
-
-    // Fermer la connexion à la base de données
-    $conn->close();
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
 }
+
+// signup.php
+require_once 'connection.php';
+
+// Démarrer la session
+session_start();
+
+// Vérifier si l'utilisateur est déjà connecté
+if (isset($_SESSION['id_user'])) {
+    // L'utilisateur est déjà connecté, rediriger vers une page sécurisée
+    header("Location: home.php");
+    exit();
+}
+
+// Récupérer les données du formulaire
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$phone = $_POST['phone'];
+$city = $_POST['city'];
+$id_role = $_POST['id_role'];
+
+// Vérifier si le mot de passe et la confirmation du mot de passe correspondent
+if ($password !== $confirm_password) {
+    die("Le mot de passe et la confirmation du mot de passe ne correspondent pas.");
+}
+
+// Hasher le mot de passe
+$password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+// Insérer l'utilisateur dans la base de données
+$sql = "INSERT INTO users (username, email, password, phone, city, id_role) VALUES (?, ?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssss", $username, $email, $password_hash, $phone, $city, $id_role);
+
+if ($stmt->execute()) {
+    // Récupérer l'ID du nouvel utilisateur inséré
+    $nouvellement_inseré_id = $conn->insert_id;
+
+    // Enregistrer les informations de l'utilisateur dans la session
+    $_SESSION['id_user'] = $nouvellement_inseré_id;
+    $_SESSION['username'] = $username;
+    $_SESSION['id_role'] = $id_role;
+
+    // Rediriger vers une page sécurisée
+    header("Location: home.php");
+    exit();
+} else {
+    echo "Erreur lors de l'inscription : " . $stmt->error;
+}
+
+// Fermer la connexion
+$stmt->close();
+$conn->close();
 ?>
